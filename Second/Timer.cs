57c922b68fddef433cfbab861d5a36269e9d06d3
David Baghdasaryan <david4443444@gmail.com>
2019-05-24 01:39:01 +0400

    Build

---
+++ Second/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second
+{
+    public static class Timer
+    {
+        public static DateTime BirthDay { get; set; }
+
+
+        public static int Yer { get; set; }
+        static Timer()
+        {
+            Console.Write("Enter your birt day...");
+            BirthDay = DateTime.Parse(Console.ReadLine());
+
+        }
+        public static void PrintAge()
+        {
+            TimeSpan ts = DateTime.Now - BirthDay;
+
+            Console.WriteLine("Age BY Milisecond" + "\t" + ts.Milliseconds);
+
+            Yer = 365;
+
+
+
+        }
+    }
+    }
